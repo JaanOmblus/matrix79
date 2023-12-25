51fad5253ad33a54f322093290d0db37cd654c9c
kvartiil <138615442+kvartiil@users.noreply.github.com>
2023-12-25 18:44:08 +0200

    Initial commit

---
+++ src/environments/environments.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  modelStruktuur: 0.4,
+  modelMaksevoime: 0.5,
+  modelRent: 0.87,
+  modelEfekt: 0.3,
+  modelLaen: 0.2,
+  mregion: 2,
+  kovKood: "0130",
+  omvNimi: "Hiiu maakond",
+  sektorKood: "23"
+};
+
+export const myNewConstant = {
+  key: 'value'
+};
